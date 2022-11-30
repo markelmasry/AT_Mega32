8dd9981245eff2bb8e6524f51d98695ff8cc2df3
markelmasry <85359577+markelmasry@users.noreply.github.com>
2022-11-30 18:00:06 +0200

    Add files via upload

--- 03-APP/Car_With_Hand_Motion/TX/main.c
+++ 03-APP/Car_With_Hand_Motion/TX/main.c
@@ -9,13 +9,12 @@
 
 #define F_CPU 16000000UL
 #include "Types.h"
-#include "LED_interface.h"
+
 #include "LCD_interface.h"
-#include "KPD_interface.h"
 #include "util/delay.h"
 #include "LM35_interface.h"
 #include "UART_interface.h"
-#include "SWT_interface.h"
+
 
 void forward(){
 	DIO_vidWritePin(GPIOC,PIN3,HIGH);
@@ -59,22 +58,20 @@ void stop(){
  u8 x,y;
 int main(void)
 {
-	//driver l293d
+//****************driver l293d*************//
 	DIO_vidSetPinMode(GPIOC,PIN3,OUTPUT);
 	DIO_vidSetPinMode(GPIOC,PIN4,OUTPUT);
 	DIO_vidSetPinMode(GPIOC,PIN5,OUTPUT);
 	DIO_vidSetPinMode(GPIOC,PIN6,OUTPUT);
 	DIO_vidSetPinMode(GPIOD,PIN4,INPUT);
 	DIO_vidSetPinMode(GPIOD,PIN5,INPUT);
-//	DIO_vidSetPinMode(GPIOD,PIN7,OUTPUT);
-//	DIO_vidWritePin(GPIOD,PIN7,HIGH);
-//**************uart**************************
+//**************uart**************************//
 	USART_vidInit(9600);
-//***************lcd*****************************	
+//***************lcd****************************//
 	DIO_vidSetPinMode(GPIOB,PIN2,OUTPUT);
 	DIO_vidWritePin(GPIOB,PIN2,LOW);
 	LCD_vidInit4bit(GPIOA,GPIOB,PIN1,GPIOB,PIN3,HIGH_NIBBLE);
-//***************dc speed control***************************
+//***************dc speed control***************************//
  	DC_vidInit(GPIOB, PIN5);
 
    
@@ -86,7 +83,7 @@ int main(void)
 		if ( (x>48)&& (x<148) && (x!='r') && (x!='l') && (x!='s') ){
 			LCD_vidClearScreen4bit(GPIOA,GPIOB,PIN1,GPIOB,PIN3,HIGH_NIBBLE);
 			LCD_voidDisplayString4bit(GPIOA,GPIOB,PIN1,GPIOB,PIN3,HIGH_NIBBLE,"forward");
-			//LCD_vidDisplayNumber4bit(GPIOA,GPIOB,PIN1,GPIOB,PIN3,HIGH_NIBBLE,x);
+			
 			y=x-48;
 			
 			LCD_vidMoveCursor4bit(GPIOA,GPIOB,PIN1,GPIOB,PIN3,HIGH_NIBBLE,2,1);
